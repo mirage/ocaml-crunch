e2be79f277a32f0cb06a3384db71b128b21a20f5
Rudi Grinberg <rudi.grinberg@gmail.com>
2017-06-19 23:41:49 -0400

    Port to jbuilder

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -1,10 +1,2 @@
-#!/usr/bin/env ocaml
 #use "topfind"
-#require "topkg"
-open Topkg
-
-let () =
-  Pkg.describe "crunch" @@ fun c ->
-  Ok [ Pkg.mllib "src/crunch.mllib";
-       Pkg.bin "src/main" ~dst:"ocaml-crunch";
-       Pkg.test "test/test" ]
+#require "topkg-jbuilder.auto"
